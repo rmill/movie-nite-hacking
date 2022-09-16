37dc6568d12260a384a66118d1a93ef8f45b07f4
Ryan Miller <rmill@blacklinesafety.com>
2022-09-16 15:30:04 -0600

    Added more emails

--- test-email/index.js
+++ test-email/index.js
@@ -5,15 +5,11 @@ const app = express()
 const port = 3000
 
 const emails = [
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'sdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf adssdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
-    {'from': 'alec a', 'subject': 'This is a password reset', 'message': 'This is a password reset sdfnlasdf jalkdf jajdsf jadf kaldjf ads', 'date': 'Sep 9' },
+    {'from': 'Alex Trebek', 'subject': '*NEW WEBSITE!*', 'message': 'Hello Mr. Gruber,\n\nWe got the new site up and running! You should have a link for EvilSite on your desktop. Try logging in there and let me know if it to your pleasure... sexual please', 'date': 'Sep 9' },
+    {'from': 'Rogers Comm.', 'subject': 'Your phone plan update', 'message': 'Greetings exalted leader,\n\nWe finally figured out what\'s wrong with your phone. I\'ve reset it and can access your voice mail with PIN: 666' , 'date': 'Sep 9' },
+    {'from': 'Fake 1', 'subject': 'subject', 'message': 'message' , 'date': 'Sep 9' },
+    {'from': 'Fake 2', 'subject': 'subject', 'message': 'message' , 'date': 'Sep 9' },
+    {'from': 'Fake 3', 'subject': 'subject', 'message': 'message' , 'date': 'Sep 9' },
 ];
 
 app.use(cors())
